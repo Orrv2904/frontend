2f7746193f1e9e0d4ac49cabeaa20e8d4ec6a322
Orrv2904 <omarrv2904@gmail.com>
2023-04-14 11:11:41 -0600

    :construction:

--- example/application.py
+++ example/application.py
@@ -1,16 +1,29 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkey'
 
 @app.route("/")
-def primera():
+def index():
+    return render_template("index.html")
+
+text = [
+    "Hamlet Aburto Sánchez",
+    "Jerath Sánchez",
+    "R2D2 Rodriguez"
+]
+
+@app.route("/first")
+def first():
     return render_template("primera.html")
 
-@app.route("/segunda")
-def segunda():
+@app.route("/second")
+def second():
     return render_template("segunda.html")
 
 
-@app.route("/tercera")
-def tercera():
+@app.route("/third")
+def third():
     return render_template("tercera.html")
