9f4bb85fbd32775d5ae8ac3871500a489fd505a9
none <none@example.com>
2023-04-14 10:44:56 -0600

    :construction:

--- example/application.py
+++ example/application.py
@@ -3,5 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def first():
+def primera():
     return render_template("primera.html")
+
+@app.route("/segunda")
+def segunda():
+    return render_template("segunda.html")
+
+
+@app.route("/tercera")
+def tercera():
+    return render_template("tercera.html")
